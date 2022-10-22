6e88724cc7a872d05488e70cf8d32e9a1dbc44de
Jariel Balberona <jariel@jariel-mbp.local>
2022-10-22 23:08:43 +0800

    fix build issue

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -13,11 +13,11 @@ const FourOhhFour = () => {
                 404
               </h1>
               <p className="mb-4 text-3xl tracking-tight font-bold text-white md:text-4xl">
-                Something's missing.
+                Something&apos;s missing.
               </p>
               <p className="mb-4 text-lg font-light text-white ">
-                Sorry, we can't find that page. You'll find lots to explore on
-                the home page.{" "}
+                Sorry, we can&apos;t find that page. You&apos;ll find lots to
+                explore on the home page.{" "}
               </p>
               <Link
                 href="/"
