7ac6df8f47db9b6f739f17edcd76b35cd45a406b
Jariel Balberona <jariel@jariel-mbp.local>
2022-10-22 22:53:21 +0800

    fix build issue

--- pages/contact-us/index.tsx
+++ pages/contact-us/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faEnvelope,
-  faPhoneSquareAlt,
-} from "@fortawesome/free-solid-svg-icons";
+
 const ContactUs = () => {
   return (
     <>
@@ -17,7 +13,7 @@ const ContactUs = () => {
         <div className="flex flex-row">
           <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 ">
-              We'd love to hear from you
+              We&apos;d love to hear from you
             </h2>
             <p className="mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl">
               Got a technical issue? Want to send feedback about a beta feature?
