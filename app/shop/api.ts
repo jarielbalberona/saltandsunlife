378d614fac87357247b1632d10ebd0e5eb44e0d4
advenjar <jariel@jariel-mbp.local>
2022-11-10 18:53:15 +0800

    debug api

--- app/shop/api.ts
+++ app/shop/api.ts
@@ -1,13 +1,9 @@
 export async function getDiveGearItemByCode(code: string) {
-  const item = await fetch(
-    `${process.env.NEXT_PUBLIC_HOST_DOMAIN}/api/dive-gears/${code}`
-  );
+  const item = await fetch(`/api/dive-gears/${code}`);
   return await item.json();
 }
 
 export async function getDiveGearItemByType(type: string) {
-  const items = await fetch(
-    `${process.env.NEXT_PUBLIC_HOST_DOMAIN}/api/dive-gears?type=${type}`
-  );
+  const items = await fetch(`/api/dive-gears?type=${type}`);
   return await items.json();
 }
